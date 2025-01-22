30c7a3a34fe580c792678ede6ac7b58d2fb2eb2a
sergioismaelvc <sirsergioismael@gmail.com>
2025-01-22 00:23:40 -0300

    refactor: remove GitHub Pages configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -13,10 +13,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
-  // Configure basePath and assetPrefix for GitHub Pages
-  basePath: "/Sergiokg6.github.io", // Use your GitHub repo name here (replace if needed)
-  assetPrefix: "/Sergiokg6.github.io/", // Ensure assets are correctly prefixed
-  output: "export", // Ensure Next.js generates a static export
 };
 
 module.exports = nextConfig;
